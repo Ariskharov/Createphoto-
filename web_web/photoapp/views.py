c412d87b6f013e7cdf62861395329cfd97715165
Ariskharov <144236300+Ariskharov@users.noreply.github.com>
2023-12-11 09:29:19 +0600

    все перепутал

--- web_web/photoapp/views.py
+++ web_web/photoapp/views.py
@@ -1,24 +1,28 @@
-
 from django.shortcuts import get_object_or_404
+
 from django.core.exceptions import PermissionDenied
+
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
 from django.urls import reverse_lazy
+
 from .models import Photo
 
 
 class PhotoListView(ListView):
-
     model = Photo
-    template_name = 'photoapp/list.html'
-    context_object_name = 'photos'
 
-# photoapp/views.py
+    template_name = 'photoapp/list.html'
+
+    context_object_name = 'photos'
 
 
 class PhotoTagListView(PhotoListView):
     template_name = 'photoapp/taglist.html'
 
+    # Custom function
     def get_tag(self):
         return self.kwargs.get('tag')
 
@@ -33,18 +37,24 @@ class PhotoTagListView(PhotoListView):
 
 class PhotoDetailView(DetailView):
     model = Photo
+
     template_name = 'photoapp/detail.html'
+
     context_object_name = 'photo'
 
 
 class PhotoCreateView(LoginRequiredMixin, CreateView):
     model = Photo
+
     fields = ['title', 'description', 'image', 'tags']
+
     template_name = 'photoapp/create.html'
+
     success_url = reverse_lazy('photo:list')
 
     def form_valid(self, form):
         form.instance.submitter = self.request.user
+
         return super().form_valid(form)
 
 
@@ -55,20 +65,26 @@ class UserIsSubmitter(UserPassesTestMixin):
         return get_object_or_404(Photo, pk=self.kwargs.get('pk'))
 
     def test_func(self):
+
         if self.request.user.is_authenticated:
             return self.request.user == self.get_photo().submitter
         else:
-            raise PermissionDenied('Sorry, you are not allowed here')
+            raise PermissionDenied('Sorry you are not allowed here')
 
 
 class PhotoUpdateView(UserIsSubmitter, UpdateView):
     template_name = 'photoapp/update.html'
+
     model = Photo
+
     fields = ['title', 'description', 'tags']
+
     success_url = reverse_lazy('photo:list')
 
 
 class PhotoDeleteView(UserIsSubmitter, DeleteView):
     template_name = 'photoapp/delete.html'
+
     model = Photo
+
     success_url = reverse_lazy('photo:list')
